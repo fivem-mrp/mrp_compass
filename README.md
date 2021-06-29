487f93cef5e2474f15a897f1cd37266ffc315e19
mufty <gubriansky@sernato.sk>
2021-06-29 20:57:48 +0200

    compas added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mrp_compass
+
+this is a fork from https://gitlab.com/MsQuerade/Compass-and-street-name-HUD
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'mufty'
+description 'MRP Compass and streenames'
+version '0.0.1'
+
+dependencies {
+    "mrp_core"
+}
+
+client_scripts {
+    'client/*.lua',
+}
